e967aa31ee0e7dd7a9e4cf770c857d2bc9890045
vanessamarely <vanessamarely@gmail.com>
2021-02-28 12:59:36 -0500

    feat: adding list and task component

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardComponent } from './board/board.component';
+import { ListComponent } from './components/list/list.component';
+import { TaskComponent } from './components/task/task.component';
 
 
 @NgModule({
-  declarations: [BoardComponent],
+  declarations: [BoardComponent, ListComponent, TaskComponent],
   imports: [
     CommonModule,
     BoardRoutingModule, 
---
+++ src/app/board/components/task/task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
