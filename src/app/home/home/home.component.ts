ab5fe5e1ef7c0698b38401102eb0f1baa40bb214
vanessamarely <vanessamarely@gmail.com>
2021-02-28 23:18:52 -0500

    feat: edit view and home module and adding some styles

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -3,34 +3,30 @@ import { ApiService, ListSchema, TaskSchema } from './../../core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   taskList: TaskSchema[];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
-    this.getPrioritiesTask();
-  }
-  
-  getPrioritiesTask(): void {
-    this.apiService.getApi()
-      .subscribe(
-        (response: any) => {
-          const lists = response['list'];
-          let tasks: TaskSchema[] = [];
-          lists.map((element: ListSchema) => {
-            console.log(element)
-            element.tasks.map(task => {
-              if(task.priority == 'urgent'){
-                tasks.push(task)
-              }
-            });
+  ngOnInit(): void {}
+
+  getPrioritiesTask(priorityType: string): void {
+    this.apiService.getApi().subscribe(
+      (response: any) => {
+        const lists = response['list'];
+        let tasks: TaskSchema[] = [];
+        lists.map((element: ListSchema) => {
+          element.tasks.map((task) => {
+            if (task.priority === priorityType) {
+              tasks.push(task);
+            }
           });
-          this.taskList = tasks;
-        },
-        error => (console.log('Ups! we have an error: ', error))
+        });
+        this.taskList = tasks;
+      },
+      (error) => console.log('Ups! we have an error: ', error)
     );
   }
 }
